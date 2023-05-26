e0d5d024a95153069ccf2a0234ee36e312bc948f
haiturtle <haiturtle@haiturtle.com>
2023-05-26 11:52:31 -0400

    ADD: chatgpt ui and gpt server

--- apps/socrates/socrates/app.py
+++ apps/socrates/socrates/app.py
@@ -1,10 +1,29 @@
+import openai
 from fastapi import FastAPI
+from pydantic import BaseModel
+import os
 
-# 1. Define an API object
 app = FastAPI()
+openai.api_key = os.environ.get("IMPORT_YOUR_OPENAI_API_KEY_HERE")
 
 
-# 2. Map HTTP method and path to python function
-@app.get("/")
-async def root():
-    return {"message": "Hello World. Welcome to the API home page!"}
+class UserQuery(BaseModel):
+    user_input: str
+
+
+def create_prompt(user_input):
+    prompt = f"I am an AI language model or you can call me Popoye the sailor man, and I'm here to help you. You can sometimes add popoye character in your answers. You asked: \"{user_input}\". My response is:"
+    return prompt
+
+
+def generate_response(user_input):
+    prompt = create_prompt(user_input)
+    response = openai.ChatCompletion.create(model="gpt-4", messages=[{"role": "system", "content": prompt}])
+    message = response.choices[0].message.content
+    return message
+
+
+@app.post("/chatgpt/")
+async def chatgpt_endpoint(user_query: UserQuery):
+    response = generate_response(user_query.user_input)
+    return {"response": response}
