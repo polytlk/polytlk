427ff89abe1134757ac18b58aeefc104ae32834e
haiturtle <haiturtle@haiturtle.com>
2023-06-24 18:02:56 -0400

    ADD: test for dialogue pinyin

--- apps/workers/eden/tests/test_schema.py
+++ apps/workers/eden/tests/test_schema.py
@@ -7,52 +7,70 @@ Strategies are defined for generating:
 - invalid pinyin words (not_pinyin_word_st),
 - valid tuples for the 'words' attribute (good_words_st),
 - invalid tuples for the 'words' attribute (bad_word_st),
-- valid tuples for the 'dialogue' attribute (good_diagloues_st).
+- valid tuples for the 'dialogue' attribute (good_dialogue_st).
 
 """
-
 import pytest
-from hypothesis import given
+from hypothesis import Verbosity, given, settings
 from hypothesis import strategies as st
 from hypothesis.strategies import builds, lists, text, tuples
 from pydantic import ValidationError
 from zhon.hanzi import characters
+from zhon.pinyin import sent
 from zhon.pinyin import word as pinyin_word
 
-from eden.chinese.schema import ChineseInterpretation
+from eden.chinese.schema import ChineseInterpretation as ChineseARI
+
+PROFILE = 'dev'
+LOCAL_MAX_EXAMPLES = 10
+
+settings.register_profile('dev', max_examples=LOCAL_MAX_EXAMPLES)
+settings.register_profile('debug', max_examples=LOCAL_MAX_EXAMPLES, verbosity=Verbosity.verbose)
+
+settings.load_profile(PROFILE)
 
 pinyin_word_st = st.from_regex(pinyin_word, fullmatch=True)
 not_pinyin_word_st = st.from_regex(characters, fullmatch=True)
 
+pinyin_sent_st = st.from_regex(sent, fullmatch=True)
+not_pinyin_sent_st = st.from_regex(characters, fullmatch=True)
+
 good_words_st = lists(tuples(text(), pinyin_word_st, text()), min_size=1)
 bad_word_st = tuples(text(), not_pinyin_word_st, text())
-good_diagloues_st = lists(tuples(text(), text(), text()), min_size=2)
+good_dialogue_st = lists(tuples(text(), pinyin_sent_st, text()), min_size=2)
+bad_dialogue_st = tuples(text(), not_pinyin_sent_st, text())
 
 
-@given(builds(ChineseInterpretation, words=good_words_st, dialogue=good_diagloues_st))
-def test_non_empty_meaning(instance: ChineseInterpretation):
+@pytest.mark.meaning
+@given(builds(ChineseARI, words=good_words_st, dialogue=good_dialogue_st))
+def test_non_empty_meaning(instance: ChineseARI):
     """Check meaning in interpretation model is not empty."""
     assert instance.meaning != ''
 
 
-@given(builds(ChineseInterpretation, words=good_words_st, dialogue=good_diagloues_st))
-def test_words_len(instance: ChineseInterpretation):
+@pytest.mark.words
+@given(builds(ChineseARI, words=good_words_st, dialogue=good_dialogue_st))
+def test_words_len(instance: ChineseARI):
     """Check each word tuple in interpretation model has a length of 3."""
     assert all(len(word) == 3 for word in instance.words)
 
 
-@given(
-    words=lists(bad_word_st, min_size=1),
-    meaning=text(),
-    dialogue=good_diagloues_st,
-)
-def test_words_pinyin(words, meaning, dialogue):
-    """Check validation error is raised when word tuple has invalid pinyin."""
-    with pytest.raises(ValidationError):
-        ChineseInterpretation(words=words, meaning=meaning, dialogue=dialogue)
-
-
-@given(builds(ChineseInterpretation, words=good_words_st, dialogue=good_diagloues_st))
-def test_dialogue_len(instance: ChineseInterpretation):
+@pytest.mark.dialogue
+@given(builds(ChineseARI, words=good_words_st, dialogue=good_dialogue_st))
+def test_dialogue_len(instance: ChineseARI):
     """Check each dialogue tuple in interpretation model has a length of 3."""
     assert all(len(dialogue) == 3 for dialogue in instance.dialogue)
+
+
+@pytest.mark.words
+@pytest.mark.xfail(raises=ValidationError, strict=True)
+@given(builds(ChineseARI, words=lists(bad_word_st, min_size=1), dialogue=good_dialogue_st))
+def test_words_pinyin(instance):
+    """Check validation error is raised when word tuple has invalid pinyin."""
+
+
+@pytest.mark.dialogue
+@pytest.mark.xfail(raises=ValidationError, strict=True)
+@given(builds(ChineseARI, words=good_words_st, dialogue=lists(bad_dialogue_st, min_size=2)))
+def test_dialogue_pinyin(instance):
+    """Check validation error is raised when word tuple has invalid pinyin."""
