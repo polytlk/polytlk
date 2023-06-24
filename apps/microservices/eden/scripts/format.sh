7ce3a6ee6f9edc32c9dd4034c45f6eea0ede52b2
haiturtle <haiturtle@haiturtle.com>
2023-06-24 06:08:48 -0400

    DELETE: serve commands

---
+++ apps/microservices/eden/scripts/format.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Formating ..."
+
+FOLDER=microservices/eden
+FILES=$(echo "$@" | tr ',' '\n' | grep $FOLDER | grep -v '.json' | sed 's#apps/$FOLDER/##')
+
+echo "Folder $FOLDER"
+echo "FILES $FILES"
+
+if [ -n "$FILES" ]; then
+  poetry run autopep8 --in-place $FILES --verbose --exit-code
+fi
