fd28e38344f5cd90b727f76d2770cfaaa4c5c2f0
haiturtle <haiturtle@haiturtle.com>
2023-06-25 13:23:35 -0400

    ADD: ionic input bar that can call task system

--- apps/web-client/src/components/InterpretContainer.tsx
+++ apps/web-client/src/components/InterpretContainer.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { IonInput, IonItem, IonLabel, IonSelect, IonSelectOption } from '@ionic/react';
+import { IonInput, IonItem, IonLabel, IonSelect, IonSelectOption, IonButton } from '@ionic/react';
 
 const LanguageSelector: React.FC<{ language: string; onLanguageChange: (language: string) => void }> = ({ language, onLanguageChange }) => (
   <IonItem>
@@ -15,12 +15,32 @@ const InterpretBar: React.FC = () => {
   const [language, setLanguage] = useState<string>('zh');
   const [text, setText] = useState<string>('');
 
+  const handleSubmit = async () => {
+    if (language === "zh") {
+      try {
+        const response = await fetch('http://localhost:8080/chinese/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ 'user_input': text })
+        });
+
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
+  };
+
   return (
     <div>
       <LanguageSelector language={language} onLanguageChange={setLanguage} />
       <IonItem>
         <IonInput value={text} placeholder="Enter Text" onIonChange={e => setText(e.detail.value + '')} clearInput/>
       </IonItem>
+      <IonButton onClick={handleSubmit}>Submit</IonButton>
     </div>
   );
 };
