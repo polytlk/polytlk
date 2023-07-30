6b91277ccb78f00ebe381428660636c45e9387bc
haiturtle <haiturtle@haiturtle.com>
2023-07-30 18:53:07 -0400

    ADD: default view iphone12

--- apps/web-client/src/pages/HomePage/Home.stories.tsx
+++ apps/web-client/src/pages/HomePage/Home.stories.tsx
@@ -1,10 +1,18 @@
 import type { Meta } from '@storybook/react';
 
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
 import Home from './Home';
 
 const Story: Meta<typeof Home> = {
   component: Home,
   title: 'Home',
+  parameters: {
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+      defaultViewport: 'iphone12promax',
+    },
+  },
 };
 export default Story;
 
