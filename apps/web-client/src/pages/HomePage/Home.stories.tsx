646638022a3c8a5ba3255a44aef9f14ebdde01c8
haiturtle <haiturtle@haiturtle.com>
2023-07-30 19:40:23 -0400

    ADD: interpreted story

--- apps/web-client/src/pages/HomePage/Home.stories.tsx
+++ apps/web-client/src/pages/HomePage/Home.stories.tsx
@@ -17,3 +17,35 @@ export const Primary: Story = {
     text: '',
   },
 };
+
+export const Interpreted: Story = {
+  args: {
+    data: {
+      words: [
+        ['你', 'nǐ', 'you'],
+        ['想', 'xiǎng', 'want'],
+        ['找', 'zhǎo', 'to find'],
+        ['我', 'wǒ', 'me'],
+        ['聊天', 'liáotiān', 'chat'],
+        ['随时', 'suíshí', 'anytime'],
+        ['都可以', 'dōu kěyǐ', 'can'],
+      ],
+      meaning: 'You can find me to chat anytime.',
+      dialogue: [
+        [
+          '你想找我聊天随时都可以吗？',
+          'Nǐ xiǎng zhǎo wǒ liáotiān suíshí dōu kěyǐ ma?',
+          'Can you find me to chat anytime?',
+        ],
+        [
+          '是的，我有什么事情想和你聊一聊。',
+          'Shì de, wǒ yǒu shénme shìqíng xiǎng hé nǐ liáo yī liáo.',
+          'Yes, I have something I want to chat with you about.',
+        ],
+      ],
+    },
+    language: 'zh',
+    loading: false,
+    text: '你想找我聊天随时都可以',
+  },
+};
