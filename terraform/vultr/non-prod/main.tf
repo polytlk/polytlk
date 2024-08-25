5a09baae72d6754e65e2b70f97ca4ed5fabe6439
haiturtle <haiturtle@haiturtle.com>
2024-08-25 09:35:02 -0400

    INIT: make cluster easy to tear down

--- terraform/vultr/non-prod/main.tf
+++ terraform/vultr/non-prod/main.tf
@@ -2,15 +2,35 @@ resource "vultr_kubernetes" "vke_np_cluster" {
   region  = "ewr"
   label   = "vke-np-cluster"
   version = "v1.31.0+1"
+
+  #  node_pools {
+  #    node_quantity = 1
+  #    plan          = "vc2-1c-2gb"
+  #    label         = "vke-default-np-node-pool"
+  #    auto_scaler   = false
+  #    min_nodes     = 1
+  #    max_nodes     = 1
+  #  }
 }
 
 # This resource must be created and attached to the cluster
 # before removing the default node from the vultr_kubernetes resource
-resource "vultr_kubernetes_node_pools" "np_node_pool" {
+resource "vultr_kubernetes_node_pools" "np_node_pool_1" {
   cluster_id    = vultr_kubernetes.vke_np_cluster.id
   node_quantity = 1
   plan          = "vc2-1c-2gb"
-  label         = "vke-np-node-pool"
+  label         = "vke-np-node-pool-1"
+  auto_scaler   = false
+  min_nodes     = 1
+  max_nodes     = 1
+}
+
+
+resource "vultr_kubernetes_node_pools" "np_node_pool_2" {
+  cluster_id    = vultr_kubernetes.vke_np_cluster.id
+  node_quantity = 1
+  plan          = "vc2-1c-2gb"
+  label         = "vke-np-node-pool-2"
   auto_scaler   = false
   min_nodes     = 1
   max_nodes     = 1
