9988c9ff652d4b7113382d5413ff60d14e82178c
haiturtle <haiturtle@haiturtle.com>
2024-08-26 18:55:43 -0400

    ADD: working eden-svc

--- apps/microservices/eden/eden/app.py
+++ apps/microservices/eden/eden/app.py
@@ -1,5 +1,5 @@
 """Perform Chinese NLP tasks for polytlk."""
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from opentelemetry.instrumentation.celery import CeleryInstrumentor
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
@@ -13,15 +13,6 @@ def create_app() -> FastAPI:
         ],
     )
 
-    @app.middleware('http')
-    async def add_version_header(request: Request, call_next):
-        print('request headers')
-        print(request.headers)
-        response = await call_next(request)
-        print('response headers')
-        print(response.headers)
-        return response
-
     app.add_middleware(
         CORSMiddleware,
         allow_origins=['http://localhost:4200', 'capacitor://localhost'],
@@ -30,8 +21,10 @@ def create_app() -> FastAPI:
         allow_headers=['*'],
     )
 
-    from eden.chinese.router import router  # noqa: WPS433
-    app.include_router(router)
+    from eden.chinese.router import router as chinese_router  # noqa: WPS433
+    from eden.health_check import router as health_router  # noqa: WPS433
+    app.include_router(chinese_router)
+    app.include_router(health_router)
 
     CeleryInstrumentor().instrument()
     FastAPIInstrumentor.instrument_app(app)
