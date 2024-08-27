7bb694fc392ccd6e28c5575a441d68746e0dfac0
haiturtle <haiturtle@haiturtle.com>
2024-08-26 20:28:50 -0400

    ADD: socrates deployment

--- apps/microservices/socrates/socrates/app.py
+++ apps/microservices/socrates/socrates/app.py
@@ -7,8 +7,10 @@ def create_app() -> FastAPI:
     """Make fastAPI app for testing purposes."""
     app = FastAPI()
 
-    from socrates.openai.router import router  # noqa: WPS433
-    app.include_router(router)
+    from socrates.health_check import router as health_router  # noqa: WPS433
+    from socrates.openai.router import router as ai_router  # noqa: WPS433
+    app.include_router(ai_router)
+    app.include_router(health_router)
 
     FastAPIInstrumentor.instrument_app(app)
 
