8780209ade854ce04c8895d732dd0708b54ba7d3
haiturtle <haiturtle@haiturtle.com>
2024-08-31 14:12:36 -0400

    IMPROVED: templating

--- apps/microservices/heimdall/scripts/template.sh
+++ apps/microservices/heimdall/scripts/template.sh
@@ -1,6 +1,6 @@
 TARGET_FOLDER="apps/microservices/heimdall/tilt"
 
-helmfile template -f helm/heimdall/helmfile.yaml --output-dir ../../$TARGET_FOLDER --output-dir-template "{{ .OutputDir }}/{{ .Release.Name}}"
+helmfile template -f helm/heimdall/helmfile.yaml.gotmpl --output-dir ../../$TARGET_FOLDER --output-dir-template "{{ .OutputDir }}/{{ .Release.Name}}"
 
 mv $TARGET_FOLDER/heimdall-apis/api/templates/* $TARGET_FOLDER/api/
 mv $TARGET_FOLDER/heimdall-deployment/deployment/templates/* $TARGET_FOLDER/deployment/
