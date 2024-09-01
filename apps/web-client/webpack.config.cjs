95a08b281e506bf344a64db4c8465f95ae61e4e0
haiturtle <haiturtle@haiturtle.com>
2024-09-01 14:32:44 -0400

    UPDATE: to latest 18

---
+++ apps/web-client/webpack.config.cjs
@@ -0,0 +1,15 @@
+const { composePlugins, withNx } = require('@nx/webpack');
+const { withReact } = require('@nx/react');
+
+// Nx plugins for webpack.
+module.exports = composePlugins(
+  withNx(),
+  withReact(),
+  (config) => {
+  // Note: This was added by an Nx migration. Webpack builds are required to have a corresponding Webpack config file.
+  // See: https://nx.dev/recipes/webpack/webpack-config-setup
+  console.log("config", config)
+
+
+  return config;
+});
