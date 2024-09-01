29779a17ac65b50a8be05c4e0e312469efc8b47d
haiturtle <haiturtle@haiturtle.com>
2024-08-31 22:27:20 -0400

    ADD: load balancer for api-gateway

--- tools/scripts/template_tyk.sh
+++ tools/scripts/template_tyk.sh
@@ -1,6 +1,6 @@
 TARGET_FOLDER="tilt/tyk"
 
-helmfile template -f helm/base/tyk/helmfile.yaml --include-crds --output-dir ../../../$TARGET_FOLDER --output-dir-template "{{ .OutputDir }}/{{ .Release.Name}}"
+helmfile template -f helm/base/tyk/helmfile.yaml.gotmpl --include-crds --output-dir ../../../$TARGET_FOLDER --output-dir-template "{{ .OutputDir }}/{{ .Release.Name}}"
 
 mv $TARGET_FOLDER/tyk-deps/dependencies/templates/* $TARGET_FOLDER/dependencies/
 mv $TARGET_FOLDER/tyk-headless/tyk-headless/templates/* $TARGET_FOLDER/gateway/
