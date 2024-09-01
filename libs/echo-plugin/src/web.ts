7fbf4ef040af6ca839979d712512de2aabbaa05a
haiturtle <haiturtle@haiturtle.com>
2024-09-01 14:58:49 -0400

    UPDATE: lint echo plugin

--- libs/echo-plugin/src/web.ts
+++ libs/echo-plugin/src/web.ts
@@ -1,32 +1,43 @@
 import type { EchoPluginPlugin } from './definitions';
+
 import { WebPlugin } from '@capacitor/core';
 
 declare global {
   var google: {
     accounts: {
       id: {
-        initialize: (opts: { client_id: string, callback: (response: { credential?: string }) => void }) => void
-        renderButton: (elem: HTMLElement, opts: { theme: string }) => void
-        prompt: () => void
-      }
-    }
-  }
+        initialize: (opts: {
+          client_id: string;
+          callback: (response: { credential?: string }) => void;
+        }) => void;
+        renderButton: (elem: HTMLElement, opts: { theme: string }) => void;
+        prompt: () => void;
+      };
+    };
+  };
 }
 
 export class EchoPluginWeb extends WebPlugin implements EchoPluginPlugin {
   constructor() {
-    super()
+    super();
 
-    let script = document.createElement('script');
-    script.src = "https://accounts.google.com/gsi/client";
+    const script = document.createElement('script');
+    script.src = 'https://accounts.google.com/gsi/client';
     script.async = true;
     document.head.appendChild(script);
   }
 
-  async renderLogin({ baseUrl, buttonElem }: { buttonElem: HTMLElement, baseUrl: string }): Promise<void> {
+  async renderLogin({
+    baseUrl,
+    buttonElem,
+  }: {
+    buttonElem: HTMLElement;
+    baseUrl: string;
+  }): Promise<void> {
     if (google?.accounts?.id) {
       google.accounts.id.initialize({
-        client_id: '540933041586-61juofou98dd54ktk134ktfec2c84gd3.apps.googleusercontent.com',
+        client_id:
+          '540933041586-61juofou98dd54ktk134ktfec2c84gd3.apps.googleusercontent.com',
         callback: async (response) => {
           if ('credential' in response) {
             const { credential } = response;
@@ -45,14 +56,14 @@ export class EchoPluginWeb extends WebPlugin implements EchoPluginPlugin {
             const { token } = await rawExchangeResponse.json();
 
             if (token) {
-              this.notifyListeners("loginResult", { token })
+              this.notifyListeners('loginResult', { token });
             }
           }
         },
       });
 
       google.accounts.id.prompt();
-      google.accounts.id.renderButton(buttonElem, { theme: "filled_blue" });
+      google.accounts.id.renderButton(buttonElem, { theme: 'filled_blue' });
     } else {
       console.error('Google script has not loaded yet');
     }
