a4078bc1c0346e603503d8ba4251d167d9557049
haiturtle <haiturtle@haiturtle.com>
2024-09-02 10:53:15 -0400

    REMOVE: pwd from nginx

--- apps/web-client/scripts/start.sh
+++ apps/web-client/scripts/start.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-echo "PRINTING WORKING DIRECTORY"
-pwd
-
 # Inject environment variables to index.html
 ./app/import-meta-env-alpine -x ./app/.env.example -p /usr/share/nginx/html/index.html || exit 1
 
